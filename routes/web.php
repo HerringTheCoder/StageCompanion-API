9cba87560fbdad793a38232d2ee4c637652516d1
ssandura <44547474+HerringTheCoder@users.noreply.github.com>
2019-10-28 12:25:07 +0100

    Initial commit with basic OAuth2 authentication implemented

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->post('auth/login',
+[
+    'uses'=>'AuthController@authenticate'
+]
+);
+
+$router->group(
+    ['middleware' => 'jwt.auth'],
+    function() use ($router) {
+        $router->get('users', function() {
+            $users = \App\User::all();
+            return response()->json($users);
+        });
+    }
+);
